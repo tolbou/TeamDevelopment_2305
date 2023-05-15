c6917369fc2d4e42886b88ab845d1401403cf48c
sasakisora <109976240+sasakisora@users.noreply.github.com>
2023-05-15 22:22:41 +0900

    プロジェクト作成

---
+++ TeamDevelopment_2305/src/main/java/com/example/demo/TeamDevelopment2305Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamDevelopment2305Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamDevelopment2305Application.class, args);
+	}
+
+}
---
+++ TeamDevelopment_2305/src/test/java/com/example/demo/TeamDevelopment2305ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamDevelopment2305ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
