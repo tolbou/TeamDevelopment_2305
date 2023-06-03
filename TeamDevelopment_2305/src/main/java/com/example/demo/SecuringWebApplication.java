c757b1d386d20fece9130ea21efe9cd37ef10d7c
kadayan3291 <127848530+kadayan3291@users.noreply.github.com>
2023-06-03 09:31:16 +0900

    login関係

---
+++ TeamDevelopment_2305/src/main/java/com/example/demo/SecuringWebApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringWebApplication {
+
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(SecuringWebApplication.class, args);
+	}
+
+}
+
+//アプリの保護？で必要らしい
